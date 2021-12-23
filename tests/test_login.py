69109c3af49941f234d7f23f80dc30dbf8f734c1
CCXXXI <ccxxxi@qq.com>
2021-12-24 05:16:14 +0800

    test: test_home2login

--- tests/test_login.py
+++ tests/test_login.py
@@ -34,3 +34,16 @@ def test_wrong_password(driver):
 
     # still at login page
     assert driver.current_url.endswith("/login")
+
+
+@pytest.mark.no_login
+@pytest.mark.skip(reason="https://e.gitee.com/ecnu_sei_hysun/issues/list?issue=I4NWCP")
+def test_home2login(driver):
+    """Redirect to /login if not logged in."""
+    driver.get(BASE_URL + "#/home")
+
+    # there is an alert
+    assert driver.find_element(By.CLASS_NAME, "el-message__content").text
+
+    # redirected to /login
+    assert driver.current_url.endswith("/login")
